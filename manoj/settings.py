073f8f5256b455d385d9097bb17758e0244ab51b
panchalamanoj <100944023+panchalamanoj@users.noreply.github.com>
2022-03-05 11:12:53 +0530

    Update settings.py

--- manoj/settings.py
+++ manoj/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = ['127.0.0.1', '.niss1.com']
 
 INSTALLED_APPS = [
     'whitenoise.runserver_nostatic',
-    'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
