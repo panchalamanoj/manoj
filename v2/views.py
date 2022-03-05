45f0238c6621b65fc72826ac00a67fbd6985fae6
panchalamanoj <100944023+panchalamanoj@users.noreply.github.com>
2022-03-05 13:50:47 +0530

    Update views.py

--- v2/views.py
+++ v2/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
-# Create your views here.
-
-def login(request):
-    return render(request, "login.html")
+from django.contrib.auth.models import user, auth
+    
+def register (request):
+	if request.method=="POST":
+		first_name = request.POST["first_name"] 
+		last_name = request.POST["last_name"]
+		username = request.POST["username"]
+		password1 = request.POST["password"]
+		password2 =  request.POST["password"]
+		email  = request.POST["email"]
+		
+		user  = user.objects.create_user(username = username, password = password1, email = email, first_name = first_name,last_name = last_name)
+		user.save();
+		print('user created')
+		return redirect('/')
+	else:
+		return render(request, "register.html")
     
