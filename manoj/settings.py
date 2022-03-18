b6bb04da73b5b569eb7c236d963c898db4bc7487
panchalamanoj <100944023+panchalamanoj@users.noreply.github.com>
2022-03-18 18:02:53 +0530

    Update settings.py

--- manoj/settings.py
+++ manoj/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'manoj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mansta', 
+        'USER': 'mansta', 
+        'PASSWORD': 'p.manoj123',
+        'HOST': 'mansta.heroku.com', 
+        'PORT': '5432',
     }
 }
 WHITENOISE_USE_FINDERS = True
