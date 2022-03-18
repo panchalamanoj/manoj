4254e6f2fe52abc94fe78020a536e5d2a35f70dc
panchalamanoj <100944023+panchalamanoj@users.noreply.github.com>
2022-03-18 23:06:34 +0530

    Update urls.py

--- manoj/urls.py
+++ manoj/urls.py
@@ -19,7 +19,7 @@ from v2 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("register", views.register, name="register"),
-    path("login", views.login, name="login"),
+    path("register/", views.register, name="register"),
+    path("login/", views.login, name="login"),
     
 ]
