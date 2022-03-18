1a31baf7bcffd86523e0dc2962db6a7e08018a9d
panchalamanoj <100944023+panchalamanoj@users.noreply.github.com>
2022-03-18 23:03:48 +0530

    Update urls.py

--- manoj/urls.py
+++ manoj/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .import views
+from v2 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
