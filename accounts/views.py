9dcf6c37506e73befea49ee4b6ee756829fc1e2f
panchalamanoj <100944023+panchalamanoj@users.noreply.github.com>
2022-03-19 15:30:17 +0530

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
 # Create your views here.
 
 
