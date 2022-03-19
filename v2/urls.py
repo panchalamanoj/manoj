6242718474016d0f7dbee9b6cb448ff00432846d
panchalamanoj <100944023+panchalamanoj@users.noreply.github.com>
2022-03-19 09:17:42 +0530

    Update urls.py

--- v2/urls.py
+++ v2/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('register/', views.register, name='register'),
-    path('login/', views.login, name='login'),
 ]    
 
 
