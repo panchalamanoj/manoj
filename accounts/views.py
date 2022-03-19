f9ed31ce471659430906419d8ecc94de12b2fd79
panchalamanoj <100944023+panchalamanoj@users.noreply.github.com>
2022-03-19 20:46:52 +0530

    Create views.py

---
+++ accounts/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+
+
+from .forms import CreateUserForm
+
+def register(request):
+       if  request.method == 'POST':
+           form = CreateUserForm(request.POST)
+           if form.is_valid():
+               form.save()
+               return redirect('login')
+       else:
+           form = CreateUserForm()
+       return render(request, 'register.html' , {'form': form})
+               
+def login(request):
+    return render(request,'login.html')
+
+
+def index (request):
+    return render(request, 'index.html')
