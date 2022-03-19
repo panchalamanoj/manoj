a4c6c3cf05a022ad14dbb755113a8cf246b22322
panchalamanoj <100944023+panchalamanoj@users.noreply.github.com>
2022-03-19 15:12:09 +0530

    Update views.py

--- v2/views.py
+++ v2/views.py
@@ -1,28 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from django.contrib.auth.models import User, auth
-    
-def register (request):
 
-    if request.method == 'POST':
-	    first_name = request.POST['first_name'] 
-	    last_name = request.POST['last_name']
-	    username = request.POST['username']
-	    password1 = request.POST['password1']
-	    password2 = request.POST['password2']
-	    email = request.POST['email']
-		
-	    user = User.objects.create_user(username = username, password = password1,email = email, first_name = first_name,last_name = last_name)
-	    user.save();
-	    print("user created")
-	    return redirect('https://mansta.herokuapp.com/')
-	
-    else:
-	    return render(request, "register.html")
-
-
-	
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'index.html')
 
 	
