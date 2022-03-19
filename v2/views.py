5ba92bebf02a5db951975ed09ca50992c9f030cd
panchalamanoj <100944023+panchalamanoj@users.noreply.github.com>
2022-03-19 09:39:28 +0530

    Update views.py

--- v2/views.py
+++ v2/views.py
@@ -13,9 +13,9 @@ def register (request):
 	    email = request.POST['email']
 		
 	    user = User.objects.create_user(username = username, password = password1,email = email, first_name = first_name,last_name = last_name)
-	    user.save():
+	    user.save();
 	    print("user created")
-	    return redirect('/')
+	    return redirect('')
 	
     else:
 	    return render(request, "register.html")
