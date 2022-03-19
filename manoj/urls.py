deccee04c078cf45818fccad9ad642ea84d2fd72
panchalamanoj <100944023+panchalamanoj@users.noreply.github.com>
2022-03-19 06:51:05 +0530

    Update urls.py

--- manoj/urls.py
+++ manoj/urls.py
@@ -14,11 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
-
+from django.urls import  path
+from v2 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('v2.urls')),
+    path('/', views.index, name='home'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login')
+         
     
 ]
