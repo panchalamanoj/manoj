0dc33bbad36ff30fa6770f869a4f2f96af13b69e
panchalamanoj <100944023+panchalamanoj@users.noreply.github.com>
2022-03-19 06:55:54 +0530

    Update urls.py

--- v2/urls.py
+++ v2/urls.py
@@ -1,8 +1,7 @@
-from django.urls import path
-from v2 import views
 
-urlpatterns = [
-      path('', views.index, name='home'),
-      path("register", views.register, name="register"),
-      path("login", views.login, name="login"),
-]
+
+
+
+
+
+
