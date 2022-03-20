318874e40d38909aca5426f63e6d25f0feb379b3
panchalamanoj <100944023+panchalamanoj@users.noreply.github.com>
2022-03-20 11:14:48 +0530

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -4,13 +4,16 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 from .forms import CreateUserForm
+from django.contrib import messages
 
 def register(request):
        if  request.method == 'POST':
            form = CreateUserForm(request.POST)
            if form.is_valid():
                form.save()
-               return redirect('')
+               user = form.cleaned_data.get('username')
+               messages.success(request, 'account was created for'+ user)
+               return redirect('login')
        else:
            form = CreateUserForm()
        return render(request, 'register.html' , {'form': form})
