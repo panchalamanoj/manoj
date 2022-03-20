64ca85822b21e12d35ef2e15d955528eb54e423c
panchalamanoj <100944023+panchalamanoj@users.noreply.github.com>
2022-03-20 11:46:00 +0530

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
 
 
 from .forms import CreateUserForm
