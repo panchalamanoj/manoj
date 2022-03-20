79aefa41c9399f4b9ee253648e319e5b97b71409
panchalamanoj <100944023+panchalamanoj@users.noreply.github.com>
2022-03-20 19:49:24 +0530

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
+from .forms import CreateUserForm 
 
 def register(request):
        if  request.method == 'POST':
